3feb0ffcf36087c7f74f65bfa2ed653642cce1a6
Kino <jyllescasruiz@gmail.com>
2015-06-15 18:37:37 +0200

    Retomando

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,21 @@
+var altoPagina = $(window).height();
+var itemsMenu = $("nav ul li ");
+var posicionMain = 0;
+
+//Despliego menu
 $("#buttonMenu").on("click", function(){
     $(this).toggleClass(" openMenu ");
     $("header").toggleClass(" headerOpen ");
     $("section.currentSection").toggleClass(" moveSection ");
 });
+//FIN Despliego menu
+
+//Scroll Menu
+itemsMenu.on("click", function(){
+    posicionMain = ($(this).index()+1) * -100 ;
+    //console.log(posicionMain);
+    $("main").animate({"top" : posicionMain+"%"}, 600, function(){
+        $("header").toggleClass(" headerOpen ");
+    });
+});
+//FIN Scroll Menu
